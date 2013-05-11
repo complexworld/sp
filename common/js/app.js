452c7f35636e6f891e5e6b3c0addb6a9e8caf4b8
kani.tsunehiro <kani.tsune@gmail.com>
2013-05-11 19:33:02 +0900

    単純な形で実装 エフェクト・動的ページが入った場合もいけるか考慮が必要

--- common/js/app.js
+++ common/js/app.js
@@ -1,60 +1,73 @@
-var contentsArea = "#about_area, #concept_area, #complex_area, #more_area";
+var contentsArea = "#menu, #about, #concept, #complex, #more";
+var current = "menu";
 
 $(function(){
     init();
-    // $(".navigation").touchstart(navAction(this));
-    $("#navigation li").click(function(){
-        $("section#navigation").animate({
-            top: -600,
-            opacity: 0
-        }, 1500);
-        var id = $(this).attr("id");
-        setTimeout(function(){
-            navAction(id);
-        }, 1500);
-        // navAction(this);
-    });
-    $("header .nav").click(function(){
-        $(contentsArea).hide();
-        navigationAnimation();
+    $("#menu li, header li").click(function(){
+        var id = $(this).attr("class");
+        if (current == id) {
+            return false;
+        }
+        viewContents(id);
+        /*
+        if (current == 'menu') {
+            $("section#menu").animate({
+                top: -600,
+                opacity: 0
+            }, 1500);
+            setTimeout(function(){
+                viewContents(id);
+            }, 1500);
+        } else {
+            viewContents(id);
+        }
+        */
     });
 });
 
 // header animation
 var initHeader = function() {
     $("header").css({top:300, left:"auto"});
-    // $("section#navigation").css({top:600, left:"auto"});
-    $("section#navigation").css({opacity: 0});
+    // $("section#menu").css({top:600, left:"auto"});
+    $("section#menu").css({opacity: 0, top: 450});
+    $("header nav").css({opacity: 0, top: 450});
     console.log("initHeader");
     setTimeout("headerAnimation()", 3000);
+    current = "menu";
 }
 var headerAnimation = function() {
     $("header").animate({
         top: 0
     }, 1500);
-    navigationAnimation();
-    $("header .nav").show("normal");
+    setTimeout(function(){
+        $("header nav").animate({
+            top: 0,
+            opacity: 1.0
+        }, 1500);
+        initMenu();
+    }, 1500);
+    // $("header nav").show("normal");
     console.log("headerAnimation");
 }
-var navigationAnimation = function() {
-    $("section#navigation").show("normal");
-    $("section#navigation").animate({
-        top: 100,
+var initMenu = function() {
+    $("section#menu").show("normal");
+    $("section#menu").animate({
+        top: 150,
         opacity: 1.0
     }, 1500);
-    console.log("navigationAnimation");
+    console.log("initMenu");
 }
 
-// nav action
-var navAction = function(id) {
+// view contents
+var viewContents = function(id) {
     $(contentsArea).hide();
-    $("#" + id + "_area").show();
+    $("#" + id).show();
+    current = id;
     console.log("view " + id + "area");
 }
 
 // init
 var init = function() {
     $(contentsArea).hide();
-    $("header .nav").hide();
     initHeader();
 }
