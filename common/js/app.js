2a0b97ab5aaa9b441d28306f2ffa3ea6d9952ace
kani.tsunehiro <kani.tsune@gmail.com>
2013-05-11 21:29:52 +0900

    no message

--- common/js/app.js
+++ common/js/app.js
@@ -1,38 +1,35 @@
+var contents = ["menu", "about", "concept", "complex"];
 var contentsArea = "#menu, #about, #concept, #complex, #more";
 var current = "menu";
 
 $(function(){
     init();
+    // TODO タッチイベントに変更
     $("#menu li, header li").click(function(){
         var id = $(this).attr("class");
         if (current == id) {
             return false;
         }
+        // コンテンツ表示
         viewContents(id);
-        /*
-        if (current == 'menu') {
-            $("section#menu").animate({
-                top: -600,
-                opacity: 0
-            }, 1500);
-            setTimeout(function(){
-                viewContents(id);
-            }, 1500);
+        if (current != "menu") {
+            // メニュー以外の場合ナビを表示
+            initNav();
         } else {
-            viewContents(id);
+            // メニューの場合ナビを非表示
+            hiddenNav();
         }
-        */
+        console.log("current=" + current);
     });
 });
 
 // header animation
 var initHeader = function() {
     $("header").css({top:300, left:"auto"});
-    // $("section#menu").css({top:600, left:"auto"});
-    $("section#menu").css({opacity: 0, top: 450});
-    $("header nav").css({opacity: 0, top: 450});
+    $("section#menu").css({opacity: 0, top: 150});
+    hiddenNav();
     console.log("initHeader");
-    setTimeout("headerAnimation()", 3000);
+    setTimeout("headerAnimation()", 2000);
     current = "menu";
 }
 var headerAnimation = function() {
@@ -40,25 +37,26 @@ var headerAnimation = function() {
         top: 0
     }, 1500);
     setTimeout(function(){
-        $("header nav").animate({
-            top: 0,
-            opacity: 1.0
-        }, 1500);
         initMenu();
-    }, 1500);
-    // $("header nav").show("normal");
+    }, 1000);
     console.log("headerAnimation");
 }
 var initMenu = function() {
     $("section#menu").show("normal");
     $("section#menu").animate({
-        top: 150,
         opacity: 1.0
     }, 1500);
     console.log("initMenu");
 }
-
-// view contents
+var initNav = function() {
+    $("header nav").animate({
+        top: 0,
+        opacity: 1.0
+    }, 1500);
+}
+var hiddenNav = function() {
+    $("header nav").css({opacity: 0});
+}
 var viewContents = function(id) {
     $(contentsArea).hide();
     $("#" + id).show();
@@ -69,5 +67,13 @@ var viewContents = function(id) {
 // init
 var init = function() {
     $(contentsArea).hide();
-    initHeader();
+    // URLのhashを見てcurrentを更新
+    var hash = window.location.hash.split("#");
+    if (hash.length == 2 && contents.indexOf(hash[1]) != -1) {
+        current = hash[1];
+        viewContents(current);
+    } else {
+        current = "menu";
+        initHeader();
+    }
 }
