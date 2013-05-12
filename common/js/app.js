960b0e565f832f54adec95bd981e9d22cfe6304f
可児 経央 <kani.tsune@gmail.com>
2013-05-12 13:03:43 +0900

    marginで位置指定 実機確認開始

--- common/js/app.js
+++ common/js/app.js
@@ -4,37 +4,19 @@ var current = "menu";
 
 $(function(){
     init();
-    // TODO タッチイベントに変更
-    $("#menu li, header li").click(function(){
-        var id = $(this).attr("class");
-        if (current == id) {
-            return false;
-        }
-        // コンテンツ表示
-        viewContents(id);
-        if (current != "menu") {
-            // メニュー以外の場合ナビを表示
-            initNav();
-        } else {
-            // メニューの場合ナビを非表示
-            hiddenNav();
-        }
-        console.log("current=" + current);
-    });
 });
 
-// header animation
 var initHeader = function() {
-    $("header").css({top:300, left:"auto"});
-    $("section#menu").css({opacity: 0, top: 150});
+    $("header").css({"margin-top":"100px", left:"auto"});
+    $("section#menu").css({opacity: 0});
     hiddenNav();
-    console.log("initHeader");
-    setTimeout("headerAnimation()", 2000);
+    setTimeout("headerAnimation()", 2500);
     current = "menu";
+    console.log("initHeader");
 }
 var headerAnimation = function() {
     $("header").animate({
-        top: 0
+        "margin-top": "5px"
     }, 1500);
     setTimeout(function(){
         initMenu();
@@ -42,36 +24,79 @@ var headerAnimation = function() {
     console.log("headerAnimation");
 }
 var initMenu = function() {
-    $("section#menu").show("normal");
+    /*
+    $("section#menu").show();
     $("section#menu").animate({
         opacity: 1.0
     }, 1500);
+*/
+    viewEnable("section#menu");
+    $("#menu li").click(contentsEvent);
+    current = "menu";
     console.log("initMenu");
 }
 var initNav = function() {
+    /*
     $("header nav").animate({
-        top: 0,
         opacity: 1.0
     }, 1500);
+*/
+    viewEnable("header nav")
+    $("header li").click(contentsEvent);
 }
 var hiddenNav = function() {
     $("header nav").css({opacity: 0});
+    $("header li").click(function(){
+        return false;
+    });
 }
 var viewContents = function(id) {
-    $(contentsArea).hide();
-    $("#" + id).show();
+    hiddenContents();
+    viewEnable("#" + id);
     current = id;
     console.log("view " + id + "area");
+}
+var hiddenContents = function() {
+    $(contentsArea).hide();
+    $(contentsArea).css({opacity: 0});
+}
+var viewEnable = function(target) {
+    $(target).show();
+    $(target).animate({
+        opacity: 1.0
+    }, 1500);
+}
+var contentsEvent = function() {
+    var id = $(this).attr("class");
+    if (current == id) {
+        return false;
+    }
+    if (id != "menu") {
+        viewContents(id);
+        // メニュー以外の場合ナビを表示
+        initNav();
+    } else {
+        // $(contentsArea).hide();
+        hiddenContents();
+        initMenu();
+        // メニューの場合ナビを非表示
+        hiddenNav();
+
+    }
+    console.log("current=" + current);
 }
 
 // init
 var init = function() {
-    $(contentsArea).hide();
+    // $(contentsArea).hide();
+    hiddenContents();
+
     // URLのhashを見てcurrentを更新
     var hash = window.location.hash.split("#");
     if (hash.length == 2 && contents.indexOf(hash[1]) != -1) {
         current = hash[1];
         viewContents(current);
+        $("header li").click(contentsEvent);
     } else {
         current = "menu";
         initHeader();
